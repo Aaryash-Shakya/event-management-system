328ad542ef16f7296f29fa9abbd88557e52692fe
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2023-12-24 18:44:40 +0545

    add: database connected and query tested

---
+++ backend/seeders/user.ts
@@ -0,0 +1,12 @@
+export const users = [
+    {
+        firstName: "Aaryash",
+        lastName: "Shakya",
+        email: "aaryashshakya1@gmail.com",
+    },
+    {
+        firstName: "Shakya",
+        lastName: "Aaryash",
+        email: "aaryashshakya2@gmail.com",
+    },
+]
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,8 +9,33 @@ app.use(cors());
 app.use(express.json());
 
 import db from "../models";
-db.sequelize.sync().then(() => {
-	app.listen(port, () => {
-		console.log("server started at port: 8000");
+import { users } from "../seeders/user";
+
+const createUser = () => {
+	users.map(user => {
+		db.User.create(user);
 	});
-});
+};
+
+
+db.sequelize
+	.sync()
+	.then(() => {
+		app.listen(port, () => {
+			console.log("server started at port: 8000");
+		});
+	})
+	.then(() => {
+		// createUser();
+
+        // get users
+        db.User.findAll()
+			.then(u => {
+				console.log('user are');
+				console.log(u);
+				// Process the fetched users as needed
+			})
+			.catch(error => {
+				console.error("Error fetching data:", error);
+			});
+	});
