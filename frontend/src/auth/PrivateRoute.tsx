323466b837234dcbb6405228c08ad7517dc1018c
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2024-02-11 18:24:12 +0545

    fix: convert PrivateRoute to use useEffect()

--- frontend/src/auth/PrivateRoute.tsx
+++ frontend/src/auth/PrivateRoute.tsx
@@ -1,27 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { isAuthenticated } from "./authIndex";
 import { Navigate, Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import { VscLoading } from "react-icons/vsc";
 
-const PrivateRoute:React.FC = async () => {
-	try {
-		const val = await isAuthenticated();
-		if (val === false) {
-			console.log("unauthorized");
-			return <Navigate to="/unauthorized" />;
-		} else {
-			console.log("authorized " + val);
-			return (
-				<>
-					<Navbar />
-					<Outlet />
-				</>
-			);
-		}
-	} catch (err) {
-		console.log(err);
+const PrivateRoute: React.FC = () => {
+	const [isAuthorized, setIsAuthorized] = useState<false | "user" | "admin" | null>(null);
+
+	useEffect(() => {
+		const checkAuthorization = async () => {
+			try {
+				const val = await isAuthenticated();
+				setIsAuthorized(val);
+			} catch (err) {
+				console.log(err);
+				setIsAuthorized(false);
+			}
+		};
+
+		checkAuthorization();
+	}, []);
+
+	if (isAuthorized === null) {
+		return (
+			<>
+				Loading <VscLoading />
+			</>
+		);
+	} else if (isAuthorized === false) {
+		console.log("unauthorized");
 		return <Navigate to="/unauthorized" />;
 	}
+
+	console.log("authorized " + isAuthorized);
+	return (
+		<>
+			<Outlet />
+		</>
+	);
 };
 
 export default PrivateRoute;
